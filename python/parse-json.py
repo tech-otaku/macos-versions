3d780f9dc67e8bd168a9a652671a46efe60ff5e6
Steve <tech-otaku@users.noreply.github.com>
2022-03-20 10:51:17 +0000

    Add Additional Information
    - Add platform information (PowerPC, i386, x86-64 and ARM64) for each
    major release.
    
    - Add Darwin's XNU kernel version for each release.

--- python/parse-json.py
+++ python/parse-json.py
@@ -6,6 +6,11 @@ try:
     with open("json/macos.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
+    print("Key:")
+    for _key in data["key"]:
+        print("    " + _key)
+    print("")
+
     print("File updated: " + data["updated"] + "\n")
     
     for _version in data["versions"]:
@@ -14,6 +19,12 @@ try:
         print("    Announced: " + _version["announced"])
         print("    Initial Release: " + _version["released"] + "\n")
 
+        print("    Platform(s):")
+        for _platform in _version["platforms"]:
+            print("        " + _platform)
+
+        print("")
+
         print("    System Requirements:")
         for _requirement in _version["requirements"]:
             print("        " + _requirement)
@@ -21,11 +32,15 @@ try:
         print("")
 
         print("    Releases:")
-        print("        Version  Build   Darwin  Released")
+        print("        Version  Build      Darwin  Kernel                Released")
         for _release in _version["releases"]:
-            print("        " + _release["version"].ljust(9) + _release["build"].ljust(8) + _release["darwin"].ljust(8) + _release["released"])
+            print("        " + _release["version"].ljust(9) + _release["build"].ljust(11) + _release["darwin"].ljust(8) + _release["kernel"].ljust(22)+ _release["released"])
 
         print("\n")
+
+    print("Key:")
+    for _key in data["key"]:
+        print("    " + _key)
 
 except FileNotFoundError as error_message:
 
